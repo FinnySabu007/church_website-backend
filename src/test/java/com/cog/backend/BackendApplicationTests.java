cc7961088efd20e86f88cbeb0bc51f59c12ae390
FinnySabu007 <123426717+Finny-Sabu@users.noreply.github.com>
2026-02-23 20:02:04 +0530

    Initial Commit - Spring Boot CRUD backend

---
+++ src/test/java/com/cog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
